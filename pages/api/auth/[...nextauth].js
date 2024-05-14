c4abf9e48f548173a3b12a78a2addb10a03ae34b
najamulsaqib <1najamulsaqib@gmail.com>
2024-05-14 15:27:26 +0100

    minor fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,5 +3,10 @@ import TwitterProvider from "next-auth/providers/twitter";
 
 export default NextAuth({
   debug: true,
-  providers: [TwitterProvider],
+  providers: [
+    TwitterProvider({
+      ClientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
+    }),
+  ],
 });
